aeb6545db47e4941d77ba95ace07c10cdab06200
Asfandyar Ashraf Malik <asfandyar@kreditech.com>
2017-02-24 17:26:03 +0100

    working soap request. congratulations

--- src/main/java/SoapRequestTest.java
+++ src/main/java/SoapRequestTest.java
@@ -3,119 +3,131 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.StringReader;
 
 class SoapRequestTest {
 
-//        System.setProperty("javax.net.ssl.trustStore", "/usr/lib/jvm/java-8-oracle/jre/lib/security/cacerts");
-//        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
-//        System.setProperty("javax.net.ssl.trustStoreType", "JKS");
-//
-//        System.setProperty("javax.net.ssl.keyStore", "../../../certs/Private.pfx");
-//        System.setProperty("javax.net.ssl.keyStorePassword", "Kreditech2017");
-//        System.setProperty("javax.net.ssl.keyStoreType", "PKCS12");
 
+    public static void getResponse(String xml) throws IOException, SAXException, ParserConfigurationException {
+
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        InputSource src = new InputSource();
+        src.setCharacterStream(new StringReader(xml));
+        Document doc = builder.parse(src);
+        String response = doc.getElementsByTagName("sResponse").item(0).getTextContent();
+        System.out.println("response: " + response);
+
+    }
 
     public static String payLoad(){
 
-        String xml =
-                "      <![CDATA[<RI_REQ xsi:noNamespaceSchemaLocation=\"RI_REQ_schema.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
-                "      <TecData>\n" +
-                "        <UserId>L******1</UserId>\n" +
-                "        <Password>k******y</Password>\n" +
-                "      </TecData>\n" +
-                "      <Header>\n" +
-                "        <FunctionCode>01001</FunctionCode>\n" +
-                "        <ReleaseOfFunctionCode>1</ReleaseOfFunctionCode>\n" +
-                "        <Service>EB</Service>\n" +
-                "      </Header>\n" +
-                "      <ContractData>\n" +
-                "        <FlagAllowedToExchange>1</FlagAllowedToExchange>\n" +
-                "        <OpNumber>tvy7y6</OpNumber>\n" +
-                "        <OpPhase>ZD</OpPhase>\n" +
-                "        <FinancingRequestDate>10092016</FinancingRequestDate>\n" +
-                "        <OpType>FL</OpType>\n" +
-                "        <NrOfInstalments>12</NrOfInstalments>\n" +
-                "        <Periodicity>M</Periodicity>\n" +
-                "        <MethodOfPayment></MethodOfPayment>\n" +
-                "        <FinancedCapital-Credit>100000</FinancedCapital-Credit>\n" +
-                "        <MonthlyInstalmentAmount></MonthlyInstalmentAmount>\n" +
-                "        <CurrencyCode>CZK</CurrencyCode>\n" +
-                "        <ChannelType>1</ChannelType>\n" +
-                "      </ContractData>\n" +
-                "      <AdditionalServices>\n" +
-                "        <AdditionalScores>\n" +
-                "          <ScoreModelId>FSI</ScoreModelId>\n" +
-                "        </AdditionalScores>\n" +
-                "        <IdCheck>0</IdCheck>\n" +
-                "      </AdditionalServices>\n" +
-                "      <CustomerType>1</CustomerType>\n" +
-                "      <Customer>\n" +
-                "        <FIPersonalCode>tvy7y6</FIPersonalCode>\n" +
-                "        <RoleCode>A</RoleCode>\n" +
-                "        <Individual>\n" +
-                "          <Name>\n" +
-                "            <PresentSurname-TradeName>Malik</PresentSurname-TradeName>\n" +
-                "            <PresentName-OwnerPresentName>Asfandyar Ashraf</PresentName-OwnerPresentName>\n" +
-                "            <NameForcing>0</NameForcing>\n" +
-                "            <PresentSurnameForcing>0</PresentSurnameForcing>\n" +
-                "            <OwnerPresentSurname>NEKONEČNÁ</OwnerPresentSurname>\n" +
-                "            <BirthSurname>Malik</BirthSurname>\n" +
-                "            <BirthSurnameForcing>0</BirthSurnameForcing>\n" +
-                "          </Name>\n" +
-                "          <PINInfo>\n" +
-                "            <PIN>ssn</PIN>\n" +
-                "            <FlagPINUsed>0</FlagPINUsed>\n" +
-                "            <FlagPINForcing>0</FlagPINForcing>\n" +
-                "          </PINInfo>\n" +
-                "          <Gender>m</Gender>\n" +
-                "          <BirthInfo>\n" +
-                "            <DateOfBirth>10091992</DateOfBirth>\n" +
-                "            <PlaceOfBirth>BROUMOV</PlaceOfBirth>\n" +
-                "            <PlaceOfBirthForcing>0</PlaceOfBirthForcing>\n" +
-                "            <CountryCode>CZ</CountryCode>\n" +
-                "          </BirthInfo>\n" +
-                "          <Address>\n" +
-                "            <Place>\n" +
-                "              <Street></Street>\n" +
-                "              <ZIP></ZIP>\n" +
-                "              <City></City>\n" +
-                "              <Region></Region>\n" +
-                "              <CountryCode></CountryCode>\n" +
-                "            </Place>\n" +
-                "          </Address>\n" +
-                "          <AdditionalAddress>\n" +
-                "            <Place>\n" +
-                "              <Street></Street>\n" +
-                "              <ZIP></ZIP>\n" +
-                "              <City></City>\n" +
-                "              <Region></Region>\n" +
-                "              <CountryCode></CountryCode>\n" +
-                "            </Place>\n" +
-                "          </AdditionalAddress>\n" +
-                "          <PhoneInfo>\n" +
-                "            <PhoneNumber></PhoneNumber>\n" +
-                "            <FaxNumber></FaxNumber>\n" +
-                "            <MobilePhone></MobilePhone>\n" +
-                "            <E-mail></E-mail>\n" +
-                "            <AdditionalPhoneNumber></AdditionalPhoneNumber>\n" +
-                "            <AdditionalFaxNumber></AdditionalFaxNumber>\n" +
-                "          </PhoneInfo>\n" +
-                "          <DocumentInfo>\n" +
-                "            <DocumentType>3</DocumentType>\n" +
-                "            <DocumentNumber>VS253005</DocumentNumber>\n" +
-                "            <DocumentIssueDate>16072014</DocumentIssueDate>\n" +
-                "            <DocumentExpirationDate></DocumentExpirationDate>\n" +
-                "            <CountryCode>CZ</CountryCode>\n" +
-                "          </DocumentInfo>\n" +
-                "          <FlagElectronicSignatureHolder>0</FlagElectronicSignatureHolder>\n" +
-                "        </Individual>\n" +
-                "      </Customer>\n" +
-                "      <ReplyTypeFlag>1</ReplyTypeFlag>\n" +
-                "      <DetailOnResidenceRequestFlag>1</DetailOnResidenceRequestFlag>\n" +
-                "      <DetailOnShareholderRequestFlag>1</DetailOnShareholderRequestFlag>\n" +
-                "      <DetailOnRelatedNamesRequestFlag>1</DetailOnRelatedNamesRequestFlag>\n" +
-                "      <DetailOnAdditionalPersonalRequestFlag>1</DetailOnAdditionalPersonalRequestFlag>\n" +
-                "    </RI_REQ>]]>\n";
+        String xml = "      \t<![CDATA[<RI_REQ xsi:noNamespaceSchemaLocation=\"RI_REQ_schema.xsd\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n" +
+                "\t\t\t<TecData>\n" +
+                "\t\t\t\t<UserId>L0045001</UserId>\n" +
+                "\t\t\t\t<Password>k9F3BgXy</Password>\n" +
+                "\t\t\t</TecData>\n" +
+                "\t\t\t<Header>\n" +
+                "\t\t\t\t<FunctionCode>01001</FunctionCode>\n" +
+                "\t\t\t\t<ReleaseOfFunctionCode>1</ReleaseOfFunctionCode>\n" +
+                "\t\t\t\t<Service>EB</Service>\n" +
+                "\t\t\t</Header>\n" +
+                "\t\t\t<ContractData>\n" +
+                "\t\t\t\t<FlagAllowedToExchange>1</FlagAllowedToExchange>\n" +
+                "\t\t\t\t<OpNumber></OpNumber>\n" +
+                "\t\t\t\t<FinancingRequestDate>10042015</FinancingRequestDate>\n" +
+                "\t\t\t\t<OpPhase>ZD</OpPhase>\n" +
+                "\t\t\t\t<OpType>FL</OpType>\n" +
+                "\t\t\t\t<NrOfInstalments>12</NrOfInstalments>\n" +
+                "\t\t\t\t<Periodicity>M</Periodicity>\n" +
+                "\t\t\t\t<MethodOfPayment></MethodOfPayment>\n" +
+                "\t\t\t\t<FinancedCapital-Credit>100000</FinancedCapital-Credit>\n" +
+                "\t\t\t\t<MonthlyInstalmentAmount></MonthlyInstalmentAmount>\n" +
+                "\t\t\t\t<CurrencyCode>CZK</CurrencyCode>\n" +
+                "\t\t\t\t<ChannelType>1</ChannelType>\n" +
+                "\t\t\t</ContractData>\n" +
+                "\t\t\t<AdditionalServices>\n" +
+                "\t\t\t\t<AdditionalScores>\n" +
+                "\t\t\t\t\t<ScoreModelId>FSI</ScoreModelId>\n" +
+                "\t\t\t\t</AdditionalScores>\n" +
+                "\t\t\t\t<IdCheck>0</IdCheck>\n" +
+                "\t\t\t</AdditionalServices>\n" +
+                "\t\t\t<CustomerType>1</CustomerType>\n" +
+                "\t\t\t<Customer>\n" +
+                "\t\t\t\t<FIPersonalCode></FIPersonalCode>\n" +
+                "\t\t\t\t<RoleCode>A</RoleCode>\n" +
+                "\t\t\t\t<Individual>\n" +
+                "\t\t\t\t\t<Name>\n" +
+                "\t\t\t\t\t\t<PresentSurname-TradeName>NEKONEČNÁ</PresentSurname-TradeName>\n" +
+                "\t\t\t\t\t\t<PresentName-OwnerPresentName>JARMILA</PresentName-OwnerPresentName>\n" +
+                "\t\t\t\t\t\t<NameForcing>0</NameForcing>\n" +
+                "\t\t\t\t\t\t<PresentSurnameForcing>0</PresentSurnameForcing>\n" +
+                "\t\t\t\t\t\t<OwnerPresentSurname>NEKONEČNÁ</OwnerPresentSurname>\n" +
+                "\t\t\t\t\t\t<BirthSurname>RYCHLÁ</BirthSurname>\n" +
+                "\t\t\t\t\t\t<BirthSurnameForcing>0</BirthSurnameForcing>\n" +
+                "\t\t\t\t\t</Name>\n" +
+                "\t\t\t\t\t<PINInfo>\n" +
+                "\t\t\t\t\t\t<PIN>756015/0125</PIN>\n" +
+                "\t\t\t\t\t\t<FlagPINUsed>0</FlagPINUsed>\n" +
+                "\t\t\t\t\t\t<FlagPINForcing>0</FlagPINForcing>\n" +
+                "\t\t\t\t\t</PINInfo>\n" +
+                "\t\t\t\t\t<Gender>Z</Gender>\n" +
+                "\t\t\t\t\t<BirthInfo>\n" +
+                "\t\t\t\t\t\t<DateOfBirth>15101975</DateOfBirth>\n" +
+                "\t\t\t\t\t\t<PlaceOfBirth>BROUMOV</PlaceOfBirth>\n" +
+                "\t\t\t\t\t\t<PlaceOfBirthForcing>0</PlaceOfBirthForcing>\n" +
+                "\t\t\t\t\t\t<CountryCode>CZ</CountryCode>\n" +
+                "\t\t\t\t\t</BirthInfo>\n" +
+                "\t\t\t\t\t<Address>\n" +
+                "\t\t\t\t\t\t<Place>\n" +
+                "\t\t\t\t\t\t\t<Street></Street>\n" +
+                "\t\t\t\t\t\t\t<ZIP></ZIP>\n" +
+                "\t\t\t\t\t\t\t<City></City>\n" +
+                "\t\t\t\t\t\t\t<Region></Region>\n" +
+                "\t\t\t\t\t\t\t<CountryCode></CountryCode>\n" +
+                "\t\t\t\t\t\t</Place>\n" +
+                "\t\t\t\t\t</Address>\n" +
+                "\t\t\t\t\t<AdditionalAddress>\n" +
+                "\t\t\t\t\t\t<Place>\n" +
+                "\t\t\t\t\t\t\t<Street></Street>\n" +
+                "\t\t\t\t\t\t\t<ZIP></ZIP>\n" +
+                "\t\t\t\t\t\t\t<City></City>\n" +
+                "\t\t\t\t\t\t\t<Region></Region>\n" +
+                "\t\t\t\t\t\t\t<CountryCode></CountryCode>\n" +
+                "\t\t\t\t\t\t</Place>\n" +
+                "\t\t\t\t\t</AdditionalAddress>\n" +
+                "\t\t\t\t\t<PhoneInfo>\n" +
+                "\t\t\t\t\t\t<PhoneNumber></PhoneNumber>\n" +
+                "\t\t\t\t\t\t<FaxNumber></FaxNumber>\n" +
+                "\t\t\t\t\t\t<MobilePhone></MobilePhone>\n" +
+                "\t\t\t\t\t\t<E-mail></E-mail>\n" +
+                "\t\t\t\t\t\t<AdditionalPhoneNumber></AdditionalPhoneNumber>\n" +
+                "\t\t\t\t\t\t<AdditionalFaxNumber></AdditionalFaxNumber>\n" +
+                "\t\t\t\t\t</PhoneInfo>\n" +
+                "\t\t\t\t\t<DocumentInfo>\n" +
+                "\t\t\t\t\t\t<DocumentType>3</DocumentType>\n" +
+                "\t\t\t\t\t\t<DocumentNumber>VS253005</DocumentNumber>\n" +
+                "\t\t\t\t\t\t<DocumentIssueDate>16072014</DocumentIssueDate>\n" +
+                "\t\t\t\t\t\t<DocumentExpirationDate></DocumentExpirationDate>\n" +
+                "\t\t\t\t\t\t<CountryCode>CZ</CountryCode>\n" +
+                "\t\t\t\t\t</DocumentInfo>\n" +
+                "\t\t\t\t\t<FlagElectronicSignatureHolder>0</FlagElectronicSignatureHolder>\n" +
+                "\t\t\t\t</Individual>\n" +
+                "\t\t\t</Customer>\n" +
+                "\t\t\t<ReplyTypeFlag>1</ReplyTypeFlag>\n" +
+                "\t\t\t<DetailOnResidenceRequestFlag>1</DetailOnResidenceRequestFlag>\n" +
+                "\t\t\t<DetailOnShareholderRequestFlag>1</DetailOnShareholderRequestFlag>\n" +
+                "\t\t\t<DetailOnRelatedNamesRequestFlag>1</DetailOnRelatedNamesRequestFlag>\n" +
+                "\t\t\t<DetailOnAdditionalPersonalRequestFlag>1</DetailOnAdditionalPersonalRequestFlag>\n" +
+                "\t\t</RI_REQ>]]>";
+
 
         return xml;
     }
@@ -125,25 +137,22 @@ class SoapRequestTest {
 
         // Create a StringEntity for the SOAP XML.
         String body = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
-                "  <soap:Body>\n" +
+                "<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">\n" +
+                "  <soap12:Body>\n" +
                 "    <processSOAPReq xmlns=\"http://tempuri.org/\">\n" +
-                "      <sRequest>\n" +
-                    SoapRequestTest.payLoad() +
+                "      <sRequest>" +
+                         SoapRequestTest.payLoad() +
                 "      </sRequest>\n" +
-                "      <sResponse></sResponse>\n" +
                 "    </processSOAPReq>\n" +
-                "  </soap:Body>\n" +
-                "</soap:Envelope>";
+                "  </soap12:Body>\n" +
+                "</soap12:Envelope>\n";
+
 
         StringEntity stringEntity = new StringEntity(body);
         stringEntity.setChunked(true);
-        System.out.println("=====================================================");
-        System.out.println("body: " + body);
-        System.out.println("=====================================================");
+//        System.out.println("body: " + body);
 
-
-        HttpPost httpPost = new HttpPost("https://www.eurisczech.com/soaplistener/eurisczech.asmx");
+        HttpPost httpPost = new HttpPost("https://www.eurisczechtest.com/soaplistener/eurisczech.asmx");
         httpPost.setEntity(stringEntity);
         httpPost.addHeader("Content-Type", "text/xml; charset=utf-8");
         httpPost.addHeader("SOAPAction", "http://tempuri.org/processSOAPReq");
@@ -153,10 +162,12 @@ class SoapRequestTest {
 
         try {
             HttpResponse response = httpClient.execute(httpPost);
-            System.out.println("StatusCode: " + response.getStatusLine().getStatusCode());
-            System.out.println("Response: " + response.toString());
-            System.out.println("Entity: " + EntityUtils.toString(response.getEntity()));
+//            System.out.println("StatusCode: " + response.getStatusLine().getStatusCode());
+//            System.out.println("Response: " + response.toString());
+            String finalResponse = EntityUtils.toString(response.getEntity());
+//            System.out.println("Entity: " + finalResponse);
             System.out.println("=====================================================");
+            getResponse(finalResponse);
 
         } finally {
             httpClient.close();
